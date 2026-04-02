588b8e9a799e771f76a7935a1c02f3f60c4aa915
ShinHa_ <ymkzz123@gmail.com>
2026-04-02 23:44:32 +0900

    bfs2차원 구현 플러드필쌀먹 드가자

--- dd.py
+++ dd.py
@@ -1,6 +1,4 @@
-print("Hello my fucked physic exam")
-#정창욱씨발새끼야
-
+from collections import deque
 
 def dfs(v):
     visited[v] = True
@@ -8,5 +6,24 @@ def dfs(v):
         if not visited[i]:
             dfs(i)
 
+
+def bfs(x, y):
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    q = deque([(x, y)])
+    
+    while q:
+        x, y = q.popleft
+        
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            
+            if 0 <= nx < N and 0 <= ny < N:
+                res += 1
+                q.append((nx, ny))
+    
 visited = [False]*10001
 g = [[], [2,3],[3,4],[1,2],[2]]
+N = int(input())
+res = 0
