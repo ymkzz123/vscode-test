2ea0e87a6595fdb7915d8a70853f07fd57fd69c6
ShinHa_ <ymkzz123@gmail.com>
2026-04-02 23:38:09 +0900

    dfs구현

--- dd.py
+++ dd.py
@@ -1,2 +1,12 @@
 print("Hello my fucked physic exam")
 #정창욱씨발새끼야
+
+
+def dfs(v):
+    visited[v] = True
+    for i in g[v]:
+        if not visited[i]:
+            dfs(i)
+
+visited = [False]*10001
+g = [[], [2,3],[3,4],[1,2],[2]]
