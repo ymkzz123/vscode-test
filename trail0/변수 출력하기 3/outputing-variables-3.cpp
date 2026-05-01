231707ab8f5b99dd79d432f60731089f563f2444
Jaeheon Lee <ymkzz123@gmail.com>
2026-05-01 21:34:12 +0900

    [Passed] 변수 출력하기 3 (8ms, 0MB)

---
+++ trail0/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    char c;
+    a = 1; b = 2;
+    c = 'C';
+    cout  << a << "->" << b << "->" << c;
+    
+    return 0;
+}
