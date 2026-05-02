34b0ecc755ca96fe25ae65e2eb640ea9859037e0
Jaeheon Lee <ymkzz123@gmail.com>
2026-05-02 16:06:12 +0900

    [Passed] 입력받은 값 교체하기 (8ms, 0MB)

---
+++ trail0/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, tmp;
+    cin >> a >> b;
+    tmp = a;
+    a = b;
+    b = tmp;
+    cout << a << " " << b;
+    return 0;
+}
